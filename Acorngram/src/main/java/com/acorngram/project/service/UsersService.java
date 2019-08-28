390ad8c9324fba50d05bb8e9f80115d99b00320a
MINJUNGYOON <MINJUNGYOON@218.38.137.28>
2019-08-28 11:20:08 +0900

    wip of usersService section and updated Servlet-context

--- Acorngram/src/main/java/com/acorngram/project/service/UsersService.java
+++ Acorngram/src/main/java/com/acorngram/project/service/UsersService.java
@@ -1,5 +1,28 @@
 package com.acorngram.project.service;
 
-public interface UsersService {
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import com.acorngram.project.dto.UsersDto;
 
+
+public interface UsersService {
+	
+	// 회원 정보 DB 저장 
+	public void addUser(UsersDto dto, ModelAndView mView);
+	// 로그인 시 아이디 유무 체크 
+	public void validUser(UsersDto dto, ModelAndView mView, HttpSession session);
+	// 회원 정보 
+	public void showInfo(HttpSession session, ModelAndView mView);
+	// 아이디 중복 체크 
+	public Map<String, Object> isExistId(String inputId);
+	// 회원 정보 삭제 
+	public void deleteUser(HttpSession session);
+	//  프로 파일 이미지를 저장하는 메서드 
+	public String saveProfileImage(HttpServletRequest request, MultipartFile mFile);
+	// 개인정보 수정 반영하는 메소드
+	public void updateUser(UsersDto dto);
+	
 }
--- Acorngram/src/main/java/com/acorngram/project/service/UsersServiceImpl.java
+++ Acorngram/src/main/java/com/acorngram/project/service/UsersServiceImpl.java
@@ -1,11 +1,70 @@
 package com.acorngram.project.service;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.acorngram.project.dao.UsersDao;
+import com.acorngram.project.dto.UsersDto;
 
 @Repository
 public class UsersServiceImpl implements UsersService{
 	@Autowired UsersDao dao;
+	
+	
+
+	// 회원 정보 DB 저장 
+	@Override
+	public void addUser(UsersDto dto, ModelAndView mView) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// 로그인 시 아이디 유무 체크 
+	@Override
+	public void validUser(UsersDto dto, ModelAndView mView, HttpSession session) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// 회원 정보 
+	@Override
+	public void showInfo(HttpSession session, ModelAndView mView) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// 아이디 중복 체크 
+	@Override
+	public Map<String, Object> isExistId(String inputId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// 회원 정보 삭제 
+	@Override
+	public void deleteUser(HttpSession session) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//  프로 파일 이미지를 저장하는 메서드 
+	@Override
+	public String saveProfileImage(HttpServletRequest request, MultipartFile mFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// 개인정보 수정 반영하는 메소드
+	@Override
+	public void updateUser(UsersDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
 }
