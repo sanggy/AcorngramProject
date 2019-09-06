a519d848936f3cbb403a92fd242c3393e6aa3e40
inqbs <incubass@outlook.com>
2019-09-06 12:01:06 +0900

    스크롤 이벤트 위치 조정

--- Acorngram/src/main/webapp/resources/@type/tl.ts
+++ Acorngram/src/main/webapp/resources/@type/tl.ts
@@ -1,63 +1,52 @@
 $(function () {
 
-    let page = sessionStorage.getItem('page')?sessionStorage.getItem('page'):1;
-    let end_flag:number = 0; 
+	let page =1;
 
-    $(window).bottom({proximity: 0.8});
+    $(window).bottom();
 
     $(window).bind("bottom", function() {
     
-        if(end_flag==0){ //ページが最後までいってなければ
+		var obj = $(this);
+	
+		if (!obj.data("loading")) {
+			obj.data("loading", true),
+			(document.querySelector('.loader'))?"":
+			$('.loading').append('<div class="loader"></div>');
+			
+				fetch('post/loadtl.do?page='+(++page))
+				.then(res=>res.text())
+				.then(res=>{
+					if(res){
+						document.querySelector('.timeline').insertAdjacentHTML('beforeend', res);
+						loadTL();
+					}
+				})
+				.catch(err=>{
+					page--;
+				})
+				.finally(()=>{
+					document.querySelector('.loader').remove();
+					obj.data("loading", false);
+				})
+				
+				/*
+				$.ajax('post/loadtl.do', { 
+					method: 'GET',
+					datatype:'html',
+					data:{page:++page},
+				}).done(response=>{
+					$(".loading").empty(); 
+					if(response!="end"){
+						$(".timeline").append($($.parseHTML(response))); 
+					}else{
+						end_flag=1;
+					}
+				}).fail(error=>{
+					page--;
+				}); //ajax
+				*/
+				
+		}
     
-			var obj = $(this);
-		
-			if (!obj.data("loading")) {
-				obj.data("loading", true),
-				(document.querySelector('.loader'))?"":
-				$('.loading').append('<div class="loader"></div>');
-		
-				setTimeout(()=> {
-					fetch('post/loadtl.do',{
-						body:{page:++page}						
-					})
-					.then(res=>res.text())
-					.then(res=>{
-						if(res){
-							document.querySelector('.timeline').insertAdjacentHTML('beforeend', res);
-							loadTL();
-						}else{
-							end_flag = 1;
-						}
-					})
-					.catch(err=>{
-						page--;
-					})
-					.finally(()=>{
-						document.querySelector('.loader').remove();
-						sessionStorage.setItem('page', page);
-						obj.data("loading", false);
-					})
-					
-					/*
-					$.ajax('post/loadtl.do', { 
-						method: 'GET',
-						datatype:'html',
-						data:{page:++page},
-					}).done(response=>{
-						$(".loading").empty(); 
-						if(response!="end"){
-							$(".timeline").append($($.parseHTML(response))); 
-						}else{
-							end_flag=1;
-						}
-					}).fail(error=>{
-						page--;
-					}); //ajax
-					*/
-					
-				}, 1000);
-			}
-    
-        } //end_flag
     });
 });
